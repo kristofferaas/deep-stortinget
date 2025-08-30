35ddfc45eb4c31a29e8861d05717298b6726ca9a
Kristoffer Aas <kristoffer@aas.dev>
2025-08-30 13:47:19 +0200

    scaffold project

---
+++ knip.ts
@@ -0,0 +1,10 @@
+import { KnipConfig } from 'knip';
+
+const config: KnipConfig = {
+  compilers: {
+    css: (text: string) => [...text.matchAll(/(?<=@)import[^;]+/g)].join('\n'),
+  },
+  ignoreDependencies: ['eslint-config-next'],
+};
+
+export default config;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-black font-geist-sans text-sm">aas.dev</h1>
+    </div>
+  );
+}
