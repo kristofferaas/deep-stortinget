75383aca4a66408394a7dcbee9488512aaf65f4f
Kristoffer Aas <kristoffer@aas.dev>
2026-02-26 07:44:25 +0100

    ci test

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import { nitro } from 'nitro/vite'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
@@ -15,4 +15,4 @@ export default defineConfig({
     // react's vite plugin must come after start's vite plugin
     viteReact(),
   ],
-})
+});
